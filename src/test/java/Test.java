0e4b23991384d7d59ccf6c2a0b531757323bf6ea
Vo14onok <Vol4onok021191@gmail.com>
2019-07-30 02:55:42 +0300

    WebSite (rozetka.com.ua) testing with selenium & cucumber

---
+++ src/test/java/Test.java
@@ -0,0 +1,13 @@
+import io.cucumber.junit.Cucumber;
+        import io.cucumber.junit.CucumberOptions;
+        import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/resources"},
+        glue = {"stepdefs"},
+        plugin = {"pretty"}
+)
+
+public class Test {
+}
---
+++ src/test/java/stepdefs/RoztkaTest.java
@@ -0,0 +1,93 @@
+package stepdefs;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+import java.util.Random;
+
+
+public class RoztkaTest {
+
+    private WebDriver driver;
+
+    @Given("^I open chrome and go to ([a-z]+.+[com]+.+[ua])$")
+    public void GoToRozetka(String arg1) {
+
+        ChromeOptions options = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        options.addArguments("start-maximized");
+        options.addArguments("disable-notifications");
+        driver = new ChromeDriver(options);
+        driver.get("http:\\" + arg1);
+
+    }
+
+    @Then("^select any city$")
+    public void SelectCity() {
+
+        int element;
+        Random r = new Random();
+
+        driver.findElement(By.xpath("//*[@class='header-cities']")).click();
+        driver.findElements(By.xpath("//*[@class='header-location__popular-link']"));
+        List<WebElement> list = driver.findElements(By.xpath("//*[@class='header-location__popular-link']"));
+        element = r.nextInt((list.size()) - 1);
+        driver.findElements(By.xpath("//*[@class='header-location__popular-link']")).get(element).click();
+
+    }
+
+    @When("^I using search field search \"([^\"\"$]*)\"$")
+    public void SearchItems(String arg1) {
+
+        driver.findElement(By.name("search")).sendKeys(arg1);
+        driver.findElement(By.name("search")).sendKeys(Keys.ENTER);
+
+    }
+
+    @Then("^select (\\d+) first results$")
+    public void SelectResults(int arg1) {
+
+        Actions actions = new Actions(driver);
+
+        List<WebElement> conditioners = driver.findElements(By.xpath("//*[@class='g-i-tile-i-box-desc']"));
+
+        for (int i = 0; i < arg1; i++ ) {
+            WebElement conditioner = conditioners.get(i);
+            actions.moveToElement(conditioner).perform();
+            driver.findElements(By.xpath("//*[@name='comparison_new_catalog']")).get(i).click();
+
+        }
+    }
+
+    @Then("^compare results$")
+    public void CompareResults() {
+
+        Actions actions = new Actions(driver);
+
+        WebElement compare = driver.findElement(By.id("comparison-header"));
+        actions.moveToElement(compare).perform();
+        driver.findElement(By.id("comparison-header")).click();
+        driver.findElement(By.cssSelector("div.btn-link-to-compare > a > span")).click();
+
+
+        //Output compare result to console
+        List<WebElement> prompt = driver.findElements(By.xpath("//*[@class='comparison-t-row']"));
+        for (WebElement item:prompt) {
+            String a = item.getText();
+            System.out.println(a);
+        }
+
+        driver.close();
+        driver.quit();
+    }
+}
