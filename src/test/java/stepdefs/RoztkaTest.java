28d9813294f02e77a4b046a5c027479d933bd718
Vo14onok <Vol4onok021191@gmail.com>
2019-08-05 00:46:46 +0300

    add additional steps + capture screenshot

--- src/test/java/stepdefs/RoztkaTest.java
+++ src/test/java/stepdefs/RoztkaTest.java
@@ -3,14 +3,16 @@ package stepdefs;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Random;
 
@@ -24,8 +26,8 @@ public class RoztkaTest {
 
         ChromeOptions options = new ChromeOptions();
         WebDriverManager.chromedriver().setup();
-        options.addArguments("start-maximized");
-        options.addArguments("disable-notifications");
+        options.addArguments("start-maximized")
+               .addArguments("disable-notifications");
         driver = new ChromeDriver(options);
         driver.get("http:\\" + arg1);
 
@@ -34,12 +36,8 @@ public class RoztkaTest {
     @Then("^select any city$")
     public void SelectCity() {
 
-        int element;
-        Random r = new Random();
-
         driver.findElement(By.cssSelector(".header-cities__link.link-dashed")).click();
-        List<WebElement> list = driver.findElements(By.cssSelector("a.header-location__popular-link"));
-        element = r.nextInt((list.size()) - 1);
+        int element = new Random().nextInt((driver.findElements(By.cssSelector("a.header-location__popular-link")).size()) - 1);
         driver.findElements(By.cssSelector("a.header-location__popular-link")).get(element).click();
 
     }
@@ -52,16 +50,33 @@ public class RoztkaTest {
 
     }
 
+    @Then("^select producer \"Boch\", \"LG\" and \"Samsung\"$")
+    public void SelectProducer() {
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name=producer_148]"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name=producer_14]"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name=producer_12]"))).click();
+
+    }
+
+    @Then("^sort by popularity$")
+    public void SortByPopularity () {
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#sort_view > a"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#filter_sortpopularity"))).click();
+//        driver.findElement(By.cssSelector("#sort_view > a")).click();
+//        driver.findElement(By.cssSelector("#filter_sortpopularity")).click();
+
+    }
+
     @Then("^select (\\d+) first results$")
     public void SelectResults(int arg1) {
 
-        Actions actions = new Actions(driver);
-
-        List<WebElement> conditioners = driver.findElements(By.cssSelector("div.over-wraper"));
-
         for (int i = 0; i < arg1; i++ ) {
-            WebElement conditioner = conditioners.get(i);
-            actions.moveToElement(conditioner).perform();
+            WebElement conditioner = driver.findElements(By.cssSelector("div.over-wraper")).get(i);
+            new Actions(driver).moveToElement(conditioner).perform();
             driver.findElements(By.cssSelector("[name=comparison_new_catalog]")).get(i).click();
 
         }
@@ -70,13 +85,10 @@ public class RoztkaTest {
     @Then("^compare results$")
     public void CompareResults() {
 
-        Actions actions = new Actions(driver);
-
         WebElement compare = driver.findElement(By.cssSelector("#comparison-header"));
-        actions.moveToElement(compare).perform();
+        new Actions(driver).moveToElement(compare).perform();
         driver.findElement(By.cssSelector("#comparison-header")).click();
         driver.findElement(By.cssSelector("div.btn-link-to-compare > a")).click();
-
 
         //Output compare result to console
         List<WebElement> prompt = driver.findElements(By.cssSelector("div.comparison-t-row"));
@@ -85,6 +97,16 @@ public class RoztkaTest {
             System.out.println(a);
         }
 
-        driver.quit();
     }
+
+    @Then("^take screenshot$")
+    public void TakeScreenshot () throws IOException {
+
+        File screenshot =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot, new File(System.getProperty("user.dir") + "\\screenshot\\" + java.time.LocalDate.now() +".jpg"));
+
+        driver.quit();
+
+    }
+
 }
--- src/test/resources/rozetka.feature
+++ src/test/resources/rozetka.feature
@@ -5,5 +5,8 @@ Feature: Compare air conditioning
     Given I open chrome and go to rozetka.com.ua
     Then select any city
     Then I using search field search "кондиционер"
-    Then select 2 first results
+    Then select producer "Boch", "LG" and "Samsung"
+    Then sort by popularity
+    Then select 3 first results
     Then compare results
+    Then take screenshot
